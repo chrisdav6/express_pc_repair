78fcbc718afb8524d6b558f3c215957a4c152736
Chris Davis <chris.davis5440@gmail.com>
2018-09-14 13:24:03 -0400

    created a json file of services and rendering it in the services route

--- routes/services.js
+++ routes/services.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fs = require("fs");
+
+let services;
+fs.readFile("json/services.json", "utf8", (err, data) => {
+  if(err) {
+    throw err;
+  } else {
+    services = JSON.parse(data);
+  }
+});
 
 /* GET services page. */
 router.get('/', (req, res, next) => {
-  res.render('services', { title: 'Services' });
+  res.render('services', { 
+    title: 'Services',
+    services: services 
+  });
 });
 
 module.exports = router;
