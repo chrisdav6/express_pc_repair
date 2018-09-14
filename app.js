9140f4a57521c780043d6df42fe7e1145b16019e
Chris Davis <chris.davis5440@gmail.com>
2018-09-14 15:38:06 -0400

    setup dotenv for email username and passwords and also configured nodemailer

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+//Require DOTENV environment variables
+require('dotenv').config();
+
 const index = require("./routes/index");
 const about = require("./routes/about");
 const services = require("./routes/services");
--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const nodemailer = require("nodemailer");
 const router = express.Router();
 
 /* GET contact page. */
 router.get('/', (req, res, next) => {
   res.render('contact', { title: 'Contact' });
+});
+
+/* POST send email */
+router.post("/send", (req, res, next) => {
+
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: SMTP_USER,
+      pass: SMTP_PASSWORD
+    }
+  });
+
+  let html = `<p>Name: ${name}</p>`;
+      html += `<p>Email: ${email}</p>`;
+      html += `<p>Message: <br> ${message}</p>`;
+
+  const mailOptions = {
+    from: '"Chris Davis ?" <chris.davis5440@gmail.com>',
+    to: '"Chris Davis ?" <chris.davis5440@gmail.com>',
+    subject: 'Hello from PC Repair MoFo!',
+    text: 'You have a submission from... Name: ' + name,
+    html: html
+  }
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("Message Sent: " + info.response);
+    res.redirect("/");
+  });
+
 });
 
 module.exports = router;
